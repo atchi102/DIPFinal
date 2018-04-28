2598e99211073dcbb6d709716637e1220266738c
atchi102 <atchi102@mail.chapman.edu>
2018-04-28 14:05:58 -0700

    load in test and training data

--- cnn.py
+++ cnn.py
@@ -1,5 +1,17 @@
-from keras.model import Sequential
-from keras.model import Conv2D
-from keras.model import MaxPooling2D
-from keras.model import Flatten
-from keras.model import Dense
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.datasets import mnist
+from matplotlib import pyplot as plt
+
+np.random.seed(123) # for reporduciility of scripts
+
+#Load pre-shuffled MNIST data into train and test datasets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+print X_train.shape
+#(60000, 28, 28): 60000 samples image are 28 by 28 pixels each
+
+plt.imshow(X_train[0])
